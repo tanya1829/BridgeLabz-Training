04e2e0593907f3cd9245a8028f872f95c0f3206d
Tanya <tanya.gla_cs22@gla.ac.in>
2025-12-19 17:02:12 +0530

    [Tanya] removed HarryAge.cs

--- core-c-sharp-practice/gcr-codebase/programming-elements/HarryAge.cs
+++
@@ -1,9 +0,0 @@
-using System;
-     class HarryAge{
-		 Static void Main(){
-			 int birthYear = 2000;
-			 int currYear = 2024;
-			 int age = currYear - birthYear;
-			 Console.WriteLine("Harry's age is " + age);
-		 }
-	 }
