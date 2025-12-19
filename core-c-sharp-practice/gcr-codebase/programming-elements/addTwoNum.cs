b7145d9a081470b4f7a22665d38baa3869981ac7
Tanya <132035563+tanya1829@users.noreply.github.com>
2025-12-19 11:07:40 +0530

    [Tanya] adding two numbers

---
+++ core-c-sharp-practice/gcr-codebase/programming-elements/addTwoNum.cs
@@ -0,0 +1,16 @@
+using System;
+
+class addTwoNum
+  
+{
+    static void Main()
+    {
+      
+        string[] inputs = Console.ReadLine().Split();
+        int a = int.Parse(inputs[0]);
+        int b = int.Parse(inputs[1]);
+        int c = a + b;
+        Console.WriteLine(c);
+      
+    }
+}
