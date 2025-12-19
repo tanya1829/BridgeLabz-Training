4d09428f6ee3b43f01d6187aa6fe694196904b4b
Tanya <tanya.gla_cs22@gla.ac.in>
2025-12-19 16:53:20 +0530

    [Tanya]Level 1 assignment

---
+++ core-c-sharp-practice/gcr-codebase/programming-elements/HarryAge.cs
@@ -0,0 +1,9 @@
+using System;
+     class HarryAge{
+		 Static void Main(){
+			 int birthYear = 2000;
+			 int currYear = 2024;
+			 int age = currYear - birthYear;
+			 Console.WriteLine("Harry's age is " + age);
+		 }
+	 }
