5538efe5c322d016d1dcae6b99a3a54e3dd87960
Tanya <tanya.gla_cs22@gla.ac.in>
2025-12-19 12:41:03 +0530

    [Tanya] Data Types

---
+++ core-c-sharp-practice/gcr-codebase/programming-elements/DataTypes.cs
@@ -0,0 +1,38 @@
+using System;
+     class DataTypes {
+         static void Main() {
+			 
+			 // Integer
+			 
+            int a = 10;
+            Console.WriteLine(a);
+			 byte b= 200;
+			Console.WriteLine(b);
+			short s=30899;
+			Console.WriteLine(s);
+			long l=12345678L;
+			
+            // Floating point
+			
+            float f = 10.5f;
+            double d = 20.75;
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+
+            // Decimal
+			
+            decimal salary = 25000.50m;
+            Console.WriteLine(salary);
+
+            // Boolean
+			
+            bool isActive = true;
+            Console.WriteLine(isActive);
+
+            // Character
+            char ch = 'A';
+            Console.WriteLine(ch);
+			string n="Tanya";
+			 Console.WriteLine(n);
+		 }
+	 }
