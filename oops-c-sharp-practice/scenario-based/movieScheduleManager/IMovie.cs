cc6b2178752377d5a95563757b158c15efa0d919
Tanya <tanya.gla_cs22@gla.ac.in>
2026-01-13 20:00:21 +0530

    [Tanya]  movieScheduleManager files

---
+++ oops-c-sharp-practice/scenario-based/movieScheduleManager/IMovie.cs
@@ -0,0 +1,10 @@
+﻿
+namespace movieScheduleManager
+{
+	interface IMovie
+	{
+		public void AddMovie();
+		public void SearchMovie();
+		public void DisplayMovie();
+	}
+}
---
+++ oops-c-sharp-practice/scenario-based/movieScheduleManager/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace movieScheduleManager
+{
+    internal class Movie
+	{
+		public string Title { get; set; }
+		public string Time { get; set; }
+
+		public Movie(string title, string time)
+		{
+			Title = title;
+			Time = time;
+		}
+	}
+}
