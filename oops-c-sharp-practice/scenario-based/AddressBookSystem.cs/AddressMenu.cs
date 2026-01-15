e71aa3d0ea1132b873074a750cfbea90cef9495c
Tanya <tanya.gla_cs22@gla.ac.in>
2026-01-15 12:34:19 +0530

    [Tanya] AddressBook System

---
+++ oops-c-sharp-practice/scenario-based/AddressBookSystem.cs/AddressMenu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    internal class AddressMenu
+    {
+        AddressBookUtility utility = new AddressBookUtility();
+        public void ShowMenu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("\n ---- Address Book ----");
+                Console.WriteLine("1. Add Contact ");
+                Console.WriteLine("2. Edit A contact");
+                Console.WriteLine("3.Delete A Contact");
+                Console.WriteLine("4.Display A contact");
+                Console.WriteLine("5.Exit");
+                Console.WriteLine("Enter you choice");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        utility.AddAContact();
+                        break;
+                    case 2:
+                        utility.EditContact();
+                        break;
+                    case 3:
+                        utility.DeleteContact();
+                        break;
+                    case 4:
+                        utility.DisplayContacts();
+                        break;
+                    case 5:
+                        Console.WriteLine("Thank you.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            } while (choice != 5);
+        }
+    }
+}
+
