c040e8a780152f2cb13c76acab9133c61861ee6a
Tanya <tanya.gla_cs22@gla.ac.in>
2026-01-19 16:31:25 +0530

    [Tanya] Scenario Based : Fitness Tracker

---
+++ data-structure-c-sharp-practice/scenario-based/FitnessTrackerr/FitnessMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTrackerr
+{
+    internal class FitnessMain
+    {
+		public static void Main(string[] args)
+		{
+			FitnessMenu menu = new FitnessMenu();
+			menu.DisplayMenu();
+
+		}
+	}
+}
---
+++ data-structure-c-sharp-practice/scenario-based/FitnessTrackerr/FitnessMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTrackerr
+{
+    internal class FitnessMenu
+    {
+		// Displays menu 
+		public void DisplayMenu()
+		{
+			FitnessUtility utility = new FitnessUtility();
+
+			while (true)
+			{
+				Console.WriteLine();
+				Console.WriteLine("  Welcome to Fitness Tracker Application ");
+
+				Console.WriteLine("Please choose an option:");
+				Console.WriteLine("1. Add a new user");
+				Console.WriteLine("2. Display steps of all users");
+				Console.WriteLine("3. Display leaderboard ");
+				Console.WriteLine("4. Exit application");
+
+				Console.Write("\nEnter your choice (1-4): ");
+				string choice = Console.ReadLine();
+
+				switch (choice)
+				{
+					case "1":
+						utility.AddUser();
+						break;
+
+					case "2":
+						utility.ShowSteps();
+						break;
+
+					case "3":
+						utility.ShowLeaderBoard();
+						break;
+
+					case "4":
+						Console.WriteLine("\nThank you for using Fitness Tracker.");
+						return;
+
+					default:
+						Console.WriteLine(" Invalid choice.");
+						break;
+				}
+			}
+		}
+	}
+}
+
+
