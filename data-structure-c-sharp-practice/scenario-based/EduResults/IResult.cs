1af474138cbfa63f26005b8978f61f28ee078821
Tanya <tanya.gla_cs22@gla.ac.in>
2026-01-21 16:29:57 +0530

    [Tanya] Edu Results

---
+++ data-structure-c-sharp-practice/scenario-based/EduResults/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduResults
+{
+	internal interface IResult
+	{
+		void AddStudent();
+		void AddDistrict();
+		void AddScore();
+		void ViewRank();
+	}
+}
---
+++ data-structure-c-sharp-practice/scenario-based/EduResults/ResultMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduResults
+{
+    internal class ResultMain
+    {
+		public static void Main(string[] args)
+		{
+			ResultMenu menu = new ResultMenu();
+			menu.ShowMenu();
+		}
+	}
+}
